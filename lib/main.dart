f8c401118f7d81e333fa285145d4bbe9f45ee172
Shaikh Ab Muiz <abdulmuiz9922@gmail.com>
2026-04-28 12:28:37 +0530

    connect firebase , Backend start

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:e_commerce/firebase_options.dart';
 import 'package:e_commerce/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  /// Widgets Flutter Binding
+  final widgetsFlutterBinding = WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-
-/// Prevent the screen from rotating
+  /// Prevent the screen from rotating
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
-
-
-
